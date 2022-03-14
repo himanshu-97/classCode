92830da04307cfaeeb38f3e81a88a9af708a8476
Himanshu Gupta <52593452+himanshu-97@users.noreply.github.com>
2022-03-14 05:36:00 +0530

    obj in JS

---
+++ module2/obj.js
@@ -0,0 +1,26 @@
+//empty obj
+let obj = {};
+console.log(obj);
+
+//adding key-value pair or data in obj
+let o = {
+    "name":"abc",
+    "age":24,
+    "Number":9999999,
+    lastName:"def"
+};
+console.log(o);
+
+let capAmerica = {
+    name:"Steve",
+    age:999,
+    friends:["Natasha","Bruce","Thor","Ironman"],
+    address:{
+        city:"Queens",
+        state:"USA"
+    },
+    saysHello:function(){
+        console.log("Hello From Cap America");
+    }
+}
+console.log(capAmerica);
