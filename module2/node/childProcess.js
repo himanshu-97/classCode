a83258c8b28310414ea782274a10f8c18fddf163
Himanshu Gupta <52593452+himanshu-97@users.noreply.github.com>
2022-03-14 18:55:32 +0530

    childProcess

--- module2/node/childProcess.js
+++ module2/node/childProcess.js
@@ -1,4 +1,8 @@
+//Same as running commands in shell but execution of both files or working of both files takes place together.
 let cp = require("child_process");
 console.log(cp);
 
-cp.execFileSync("calc");
+// cp.execSync("open -a Calculator"); // to open cal in mac
+
+let content = cp.execSync("node test.js");
+console.log("Output of test.js "+content); 
