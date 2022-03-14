42a9fca7f41ba6af19e8d487036b9acfcde76bf1
Himanshu Gupta <52593452+himanshu-97@users.noreply.github.com>
2022-03-14 07:46:18 +0530

    os intro

---
+++ module2/node/os.js
@@ -0,0 +1,11 @@
+let os = require("os");
+console.log(os);
+
+//checking hostname through os obj
+console.log(os.hostname());
+
+console.log(os.arch());
+
+console.log(os.networkInterfaces());
+
+console.log(os.userInfo());
