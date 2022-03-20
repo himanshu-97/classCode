283cc74497a22c9b860131a8e39283005cf56afc
Himanshu Gupta <52593452+himanshu-97@users.noreply.github.com>
2022-03-20 16:39:13 +0530

    Create q5.js

---
+++ module2/node/questions/q5.js
@@ -0,0 +1,12 @@
+//move a file
+
+let fs = require("fs");
+let path = require("path");
+
+let srcPath = path.join(__dirname,"move.txt");
+let desPath = path.join(__dirname,"..","move.txt");
+
+fs.copyFileSync(srcPath,desPath);
+
+fs.unlinkSync(srcPath);
+
