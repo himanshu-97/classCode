3540d0e90dbe18e10468fbbc5064d0b9e1c54af5
Himanshu Gupta <52593452+himanshu-97@users.noreply.github.com>
2022-03-20 15:17:49 +0530

    Q2 DONE

--- module2/node/questions/q2.js
+++ module2/node/questions/q2.js
@@ -20,5 +20,37 @@ for(let j =0; j<7;j++){
     }
 
 }
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+//OTHER WAY - 
+
+
+let fs = require("fs");
+let path = require("path");
+
+let arr = ["audio","video","image","software","documents","applications","other"];
+let fileName = ["abc","def","hij","klm"];
+let fileExtName = [".mp3",".mp4",".jpg",".exe",".pdf",".apk","rar"];
+
+organiseFolderPath = path.join(__dirname,"organised");
+if(!fs.existsSync(organiseFolderPath)){
+    fs.mkdirSync(organiseFolderPath);
+}
+else{
+    // console.log("Same Name Folder");
+}
+for(let i = 0; i<arr.length; i++){
+let folderPath = path.join(organiseFolderPath,arr[i]);
+{
+    if(!fs.existsSync(folderPath)){
+        fs.mkdirSync(folderPath);
+    }
+}
+for(let j = 0;j<fileName.length; j++){
+let fileAdd = fileName[j]+fileExtName[i];
+let filePath = path.join(folderPath,fileAdd);
+fs.writeFileSync(filePath,"");
+}
+}
+
 
 
